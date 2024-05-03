19895ea8ce0355b403f5f8b11a0a0f574f7b809c
joshuaboud <joshuaboud@hotmail.com>
2024-05-03 11:27:18 -0300

    constrain notification area to below header

--- houston-common-ui/lib/components/HoustonAppContainer.vue
+++ houston-common-ui/lib/components/HoustonAppContainer.vue
@@ -3,33 +3,44 @@ import { defineProps, type Component, type PropType } from "vue";
 import HoustonHeader from "@/components/HoustonHeader.vue";
 import { defineHoustonAppTabState, type HoustonAppTabEntrySpec, TabSelector, TabView } from '@/components/tabs';
 import NotificationView from "@/components/NotificationView.vue";
+import { useGlobalProcessingState } from '@/composables/useGlobalProcessingState';
 
 const props = defineProps<{
     moduleName: string,
     tabs?: HoustonAppTabEntrySpec[];
 }>();
 
-const tabState = props.tabs !== undefined ? defineHoustonAppTabState(props.tabs) : null;
+const tabState = (props.tabs !== undefined) ? defineHoustonAppTabState(props.tabs) : null;
+
+const { globalProcessingState } = useGlobalProcessingState();
 </script>
 
 <template>
-    <div class="flex flex-col text-default h-full">
+    <div
+        class="text-default flex flex-col h-full"
+        :class="{ 'cursor-wait': globalProcessingState }"
+    >
         <HoustonHeader :moduleName="moduleName">
-            <template v-slot:header-left v-if="tabState">
+            <template
+                v-slot:header-left
+                v-if="tabState"
+            >
                 <TabSelector :state="tabState" />
             </template>
         </HoustonHeader>
-        <div class="overflow-hidden grow basis-0 flex items-stretch">
+        <NotificationView class="overflow-hidden grow basis-0 flex items-stretch">
             <div
                 class="bg-well overflow-y-auto grow"
                 style="scrollbar-gutter: stable both-edges;"
             >
-            <slot>
-                <TabView v-if="tabState" :state="tabState" />
-            </slot>
+                <slot>
+                    <TabView
+                        v-if="tabState"
+                        :state="tabState"
+                    />
+                </slot>
             </div>
-        </div>
-        <NotificationView />
+        </NotificationView>
     </div>
 </template>
 

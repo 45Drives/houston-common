8df3d4b5afec1ade8015445b09998507ac4f6fca
joshuaboud <joshuaboud@hotmail.com>
2024-05-16 12:31:27 -0300

    reference cockpit types from houston-common-lib import

--- cockpit-typings/cockpit-import-hack.d.ts
+++ cockpit-typings/cockpit-import-hack.d.ts
@@ -1,8 +1,6 @@
 /// <reference path="./cockpit.d.ts" />
-/// <reference path="./cockpit-extra.d.ts" />
 
-import { default as CockpitModule } from "cockpit";
-
+import CockpitModule from 'cockpit';
 declare global {
   var cockpit: typeof CockpitModule;
 }
--- houston-common-lib/vite.config.ts
+++ houston-common-lib/vite.config.ts
@@ -1,50 +1,52 @@
 // vite.config.js
-import { InlineConfig as VitestInlineConfig } from 'vitest';
-import dts from 'vite-plugin-dts';
+import { InlineConfig as VitestInlineConfig } from "vitest";
+import dts from "vite-plugin-dts";
 
-declare module 'vite' {
-    interface UserConfig {
-        /**
-         * Options for Vitest
-         */
-        test?: VitestInlineConfig;
-    }
+declare module "vite" {
+  interface UserConfig {
+    /**
+     * Options for Vitest
+     */
+    test?: VitestInlineConfig;
+  }
 }
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
-import { resolve } from 'path';
+import { resolve } from "path";
 
 export default defineConfig({
-	base: './',
-	resolve: {
-		alias: {
-			'@': new URL('./lib/', import.meta.url).pathname
-		}
-	},
-	build: {
-		// lib property is set in build script
-		lib: {
-			entry: new URL('./lib/index.ts', import.meta.url).pathname,
-			name: 'Houston Common Library',
-			// the proper extensions will be added
-			fileName: 'index',
-			formats: ['es', 'cjs']
-		},
-		sourcemap: true,
-	},
-	test: {
-		globals: true,
-	},
-	plugins: [
-		dts({
-			copyDtsFiles: true,
-			rollupTypes: true,
-			beforeWriteFile(filePath, content) {
-				return {
-					filePath,
-					content: /import.*?\?raw/.test(content) ? `/// <reference types="vite/client" />\n` + content : content,
-				}
-			},
-		}),
-	]
+  base: "./",
+  resolve: {
+    alias: {
+      "@": new URL("./lib/", import.meta.url).pathname,
+    },
+  },
+  build: {
+    // lib property is set in build script
+    lib: {
+      entry: new URL("./lib/index.ts", import.meta.url).pathname,
+      name: "Houston Common Library",
+      // the proper extensions will be added
+      fileName: "index",
+      formats: ["es", "cjs"],
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  test: {
+    globals: true,
+  },
+  plugins: [
+    dts({
+      copyDtsFiles: true,
+      beforeWriteFile(filePath, content) {
+        return {
+          filePath,
+          content: /import.*?\?raw/.test(content)
+            ? `/// <reference types="vite/client" />\n` + content
+            : content,
+        };
+      },
+    }),
+  ],
 });

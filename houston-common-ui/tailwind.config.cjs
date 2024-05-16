022896e96f8983cae290b3601b18522f7fc847f8
joshuaboud <joshuaboud@hotmail.com>
2024-05-16 18:04:36 -0300

    fix css export and upgrade deps

--- houston-common-ui/tailwind.config.cjs
+++ houston-common-ui/tailwind.config.cjs
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
 		"./index.html",
@@ -19,5 +20,5 @@ module.exports = {
 	plugins: [
 		require('@tailwindcss/forms'),
 	],
-	darkMode: "class",
-}
+	darkMode: 'class',
+};

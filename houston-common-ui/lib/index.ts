2baf53065cfd8333b421dc936de90187be888d03
joshuaboud <joshuaboud@hotmail.com>
2024-05-24 14:15:01 -0300

    update exports

--- houston-common-ui/lib/index.ts
+++ houston-common-ui/lib/index.ts
@@ -1,19 +1,15 @@
 export { default as HoustonHeader } from "@/components/HoustonHeader.vue";
 export { default as LoadingSpinner } from "@/components/LoadingSpinner.vue";
-export { default as ModalConfirm } from "@/components/old/ModalConfirm.vue";
-export { default as ModalPopup } from "@/components/old/ModalPopup.vue";
-export { default as DynamicFormRenderer } from "@/components/old/DynamicFormRenderer.vue";
-export { default as ModalPrompt } from "@/components/old/ModalPrompt.vue";
-export { default as DynamicForm } from "@/components/old/DynamicForm.vue";
 export { default as NotificationView } from "@/components/NotificationView.vue";
 export * from "@/components/NotificationView.vue";
 export { default as Logo45Drives } from "@/components/Logo45Drives.vue";
+export * from "@/components/HoustonAppContainer.vue";
 export { default as HoustonAppContainer } from "@/components/HoustonAppContainer.vue";
 export { default as CardContainer } from "@/components/CardContainer.vue";
 export { default as CenteredCardColumn } from "@/components/CenteredCardColumn.vue";
 export { default as ParsedTextArea } from "@/components/ParsedTextArea.vue";
-export * from '@/components/InputFeedback.vue';
-export { default as InputFeedback } from '@/components/InputFeedback.vue';
+export * from "@/components/InputFeedback.vue";
+export { default as InputFeedback } from "@/components/InputFeedback.vue";
 export * from "@/components/InputField.vue";
 export { default as InputField } from "@/components/InputField.vue";
 export { default as ToggleSwitch } from "@/components/ToggleSwitch.vue";
@@ -26,6 +22,8 @@ export { default as InputLabelWrapper } from "@/components/InputLabelWrapper.vue
 
 export * from "@/components/tabs";
 
+export * from "@/components/modals";
+
 export * from "@/composables/useDynamicFormGeneration";
 
 export * from "@/composables/useDynamicFormGenerationPromise";
@@ -34,3 +32,15 @@ export * from "@/composables/useDarkModeState";
 export * from "@/composables/useGlobalProcessingState";
 export * from "@/composables/useTempObjectStaging";
 export * from "@/composables/wrapActions";
+
+import { reportError } from '@/components/NotificationView.vue';
+
+window.onerror = (event) => {
+    if (typeof event === "string") {
+        reportError(new Error(event));
+    } else {
+        const errorEvent = event as ErrorEvent;
+        reportError(new Error(`${errorEvent.filename}:${errorEvent.lineno}:${errorEvent.colno}: ${errorEvent.message}`));
+    }
+    return false;
+}

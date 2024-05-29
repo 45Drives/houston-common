3a4c25dd7154c7a840bb8708dfab21aa31e61be9
joshuaboud <joshuaboud@hotmail.com>
2024-05-29 11:51:58 -0300

    remove InputLabelWrapper from being included within template

--- houston-common-ui/lib/components/InputField.vue
+++ houston-common-ui/lib/components/InputField.vue
@@ -1,13 +1,22 @@
 <script lang="ts">
-import { type Feedback } from '@/components/InputFeedback.vue';
-export type InputValidator = (value: string) => Feedback | undefined | PromiseLike<Feedback | undefined>;
+import { type Feedback } from "@/components/InputFeedback.vue";
+export type InputValidator = (
+  value: string
+) => Feedback | undefined | PromiseLike<Feedback | undefined>;
 </script>
 
 <script setup lang="ts">
-import { defineProps, defineModel, defineEmits, computed, defineExpose, ref, watchEffect, onMounted } from "vue";
-import { ExclamationCircleIcon, ExclamationTriangleIcon } from "@heroicons/vue/20/solid";
-import InputLabelWrapper from './InputLabelWrapper.vue';
-import InputFeedback from '@/components/InputFeedback.vue';
+import {
+  defineProps,
+  defineModel,
+  defineEmits,
+  computed,
+  defineExpose,
+  ref,
+  watchEffect,
+  onMounted,
+} from "vue";
+import InputFeedback from "@/components/InputFeedback.vue";
 import { v4 as uuidv4 } from "uuid";
 
 const props = defineProps<{
@@ -20,70 +29,49 @@ const props = defineProps<{
 const model = defineModel<string>({ default: "" });
 
 const emit = defineEmits<{
-  (e: 'input', value: string): void,
-  (e: 'change', value: string): void,
+  (e: "input", value: string): void;
+  (e: "change", value: string): void;
 }>();
 
 const feedback = ref<Feedback>();
 const updateFeedback = async () =>
-  feedback.value = await props.validator?.(model.value);
+  (feedback.value = await props.validator?.(model.value));
 watchEffect(updateFeedback);
 
 const suggestionListId = ref<string>();
-onMounted(() => suggestionListId.value = uuidv4());
+onMounted(() => (suggestionListId.value = uuidv4()));
 
 const valid = computed<boolean>(() => !(feedback.value?.type === "error"));
 
 defineExpose({
   valid,
 });
-
 </script>
 
 <template>
-  <InputLabelWrapper>
-    <template
-      v-slot:label
-      v-if="$slots.default"
-    >
-      <slot />
-    </template>
-    <template
-      v-slot:tooltip
-      v-if="$slots.tooltip"
-    >
-      <slot name="tooltip" />
-    </template>
-    <input
-      type="text"
-      name="label"
-      class="w-full input-textlike"
-      :placeholder="placeholder"
-      :disabled="disabled"
-      v-model="model"
-      @input="emit('input', model)"
-      @change="emit('change', model)"
-      :list="suggestionListId"
-      autocomplete="off"
-    />
-    <datalist
-      v-if="suggestions"
-      :id="suggestionListId"
-    >
-      <option
-        v-for="suggestion in suggestions"
-        :value="suggestion"
-      ></option>
-    </datalist>
-    <InputFeedback
-      v-if="feedback"
-      :type="feedback.type"
-      :actions="feedback.actions"
-      @feedbackAction="updateFeedback"
-    >
-      {{ feedback.message }}
-    </InputFeedback>
-  </InputLabelWrapper>
+  <input
+    type="text"
+    name="label"
+    class="w-full input-textlike"
+    :placeholder="placeholder"
+    :disabled="disabled"
+    v-model="model"
+    @input="emit('input', model)"
+    @change="emit('change', model)"
+    :list="suggestionListId"
+    autocomplete="off"
+  />
+  <datalist v-if="suggestions" :id="suggestionListId">
+    <option v-for="suggestion in suggestions" :value="suggestion"></option>
+  </datalist>
+  <InputFeedback
+    v-if="feedback"
+    :type="feedback.type"
+    :actions="feedback.actions"
+    @feedbackAction="updateFeedback"
+  >
+    {{ feedback.message }}
+  </InputFeedback>
 </template>
 
 <style scoped>

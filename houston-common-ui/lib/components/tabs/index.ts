e1d8ae5e1412bd4bf9065fda23975510b68a9794
joshuaboud <joshuaboud@hotmail.com>
2024-06-07 14:53:00 -0300

    remember last tab

--- houston-common-ui/lib/components/tabs/index.ts
+++ houston-common-ui/lib/components/tabs/index.ts
@@ -1,4 +1,4 @@
-import { ref, type Component, type Ref } from "vue";
+import { ref, watch, type Component, type Ref } from "vue";
 export { default as TabSelector } from "./TabSelector.vue";
 export { default as TabView } from "./TabView.vue";
 
@@ -19,8 +19,17 @@ export type HoustonAppTabState = {
 export function defineHoustonAppTabState(
   entries: HoustonAppTabEntrySpec[]
 ): HoustonAppTabState {
+  const lastIndex = parseInt(
+    cockpit.localStorage.getItem("HoustonHeaderTabIndex") ?? ""
+  );
+  const index = ref(
+    isNaN(lastIndex) ? 0 : Math.min(lastIndex, entries.length - 1)
+  );
+  watch(index, (newIndex) => {
+    cockpit.localStorage.setItem("HoustonHeaderTabIndex", newIndex.toString());
+  });
   return {
-    entries: entries.map(e => ({...e, key: Symbol()})),
-    index: ref(0),
+    entries: entries.map((e) => ({ ...e, key: Symbol() })),
+    index,
   };
 }

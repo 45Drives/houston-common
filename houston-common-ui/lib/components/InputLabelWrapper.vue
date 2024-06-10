2f48c663a2fe0a61efce4d1e00f6081137adc9f3
joshuaboud <joshuaboud@hotmail.com>
2024-06-10 17:31:10 -0300

    try fix tooltip

--- houston-common-ui/lib/components/InputLabelWrapper.vue
+++ houston-common-ui/lib/components/InputLabelWrapper.vue
@@ -1,37 +1,38 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import ToolTip from '@/components/ToolTip.vue';
+import { ref, defineProps } from "vue";
+import ToolTip from "@/components/ToolTip.vue";
 
 const showToolTip = ref(false);
 
+defineProps<{
+  tooltipAbove?: boolean;
+}>();
 </script>
 
 <template>
-    <div>
-        <div
-            v-if="$slots.label || $slots.tooltip"
-            class="flex items-start"
-        >
-            <label
-                v-if="$slots.label"
-                class="text-label"
-                :class="{ 'underline decoration-dotted': $slots.tooltip }"
-                @mouseenter="showToolTip = true"
-                @mouseleave="showToolTip = false"
-                @click="showToolTip = !showToolTip"
-            >
-                <slot name="label" />
-            </label>
-            <ToolTip
-                v-if="$slots.tooltip"
-                v-model="showToolTip"
-                :class="{ 'pl-1': $slots.label }"
-            >
-                <slot name="tooltip" />
-            </ToolTip>
-        </div>
-        <slot />
+  <div>
+    <div v-if="$slots.label || $slots.tooltip" class="flex items-start">
+      <label
+        v-if="$slots.label"
+        class="text-label"
+        :class="{ 'underline decoration-dotted': $slots.tooltip }"
+        @mouseenter="showToolTip = true"
+        @mouseleave="showToolTip = false"
+        @click="showToolTip = !showToolTip"
+      >
+        <slot name="label" />
+      </label>
+      <ToolTip
+        :above="tooltipAbove"
+        v-if="$slots.tooltip"
+        v-model="showToolTip"
+        :class="{ 'pl-1': $slots.label }"
+      >
+        <slot name="tooltip" />
+      </ToolTip>
     </div>
+    <slot />
+  </div>
 </template>
 
 <style scoped>
--- houston-common-ui/lib/components/ToolTip.vue
+++ houston-common-ui/lib/components/ToolTip.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-
 import { defineModel, defineProps } from "vue";
 import { QuestionMarkCircleIcon } from "@heroicons/vue/20/solid";
 
@@ -7,19 +6,18 @@ const show = defineModel<boolean>({ default: false });
 defineProps<{
   above?: boolean;
 }>();
-
 </script>
 
 <template>
   <span
-    class="relative overflow-visible inline-block"
-    @click="show = !show"
+    class="relative overflow-visible inline-block z-50"
+    @click.stop="show = !show"
     @mouseenter="show = true"
     @mouseleave="show = false"
   >
     <QuestionMarkCircleIcon class="size-icon icon-default" />
     <div
-      class="text-left absolute bg-default shadow-sm border border-default text-muted font-normal whitespace-pre-wrap text-sm rounded-md p-2 z-50 min-w-40 max-w-full"
+      class="text-left absolute bg-default shadow-sm border border-default text-muted font-normal whitespace-pre-wrap text-sm rounded-md p-2 z-50 overflow-y-auto max-h-52"
       :class="{ 'bottom-5': above }"
       v-if="show"
     >

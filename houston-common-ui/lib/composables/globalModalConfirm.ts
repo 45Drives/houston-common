f668d3ddea6e6d93da8a820eb88a877d5f0600d0
joshuaboud <joshuaboud@hotmail.com>
2024-07-04 15:05:23 -0300

    fix confirmBeforeAction

--- houston-common-ui/lib/composables/globalModalConfirm.ts
+++ houston-common-ui/lib/composables/globalModalConfirm.ts
@@ -1,47 +1,54 @@
 import { type ConfirmOptions } from "@/components/modals";
-import { ResultAsync } from "neverthrow";
+import { ResultAsync, okAsync } from "neverthrow";
 import { type Action } from "@/composables/wrapActions";
 import { ref } from "vue";
-import { type ValueElseUndefiend } from "@45drives/houston-common-lib";
+import { type CancelledByUser, type ValueElseUndefiend } from "@45drives/houston-common-lib";
 
 export type GlobalModalConfirmFunctions = {
   confirm: (options: ConfirmOptions) => ResultAsync<boolean, never>;
-  confirmBeforeAction: (
-    options: ConfirmOptions,
-    action: Action<any, any, any>
-  ) => typeof action;
   assertConfirm: <T>(
     options: ConfirmOptions,
     resultIfConfirmed?: T
-  ) => ResultAsync<ValueElseUndefiend<T>, Error>;
+  ) => ResultAsync<ValueElseUndefiend<T>, CancelledByUser>;
 };
 
-const globalModalConfirmFuncs = ref<GlobalModalConfirmFunctions>();
+// const globalModalConfirmFuncs = ref<GlobalModalConfirmFunctions>();
+
+let globalModalConfirmFuncsPromiseResolver: (funcs: GlobalModalConfirmFunctions) => void;
+
+const globalModalConfirmFuncsPromise = new Promise<GlobalModalConfirmFunctions>(
+  (resolve) => (globalModalConfirmFuncsPromiseResolver = resolve)
+);
 
 export namespace _internal {
-  export const provideGlobalModalFuncs = (
-    funcs: GlobalModalConfirmFunctions
-  ) => {
-    globalModalConfirmFuncs.value = funcs;
+  export const provideGlobalModalFuncs = (funcs: GlobalModalConfirmFunctions) => {
+    globalModalConfirmFuncsPromiseResolver(funcs);
+    // globalModalConfirmFuncs.value = funcs;
   };
 }
 
 const getGlobalModalConfirmFuncs = () => {
-  if (globalModalConfirmFuncs.value === undefined) {
-    throw new Error("Global ModalConfirm methods not provided!");
-  }
-  return globalModalConfirmFuncs.value;
+  // if (globalModalConfirmFuncs.value === undefined) {
+  //   throw new Error("Global ModalConfirm methods not provided!");
+  // }
+  // return globalModalConfirmFuncs.value;
+  return ResultAsync.fromSafePromise(globalModalConfirmFuncsPromise);
 };
 
 export const confirm = (
   ...args: Parameters<GlobalModalConfirmFunctions["confirm"]>
 ): ReturnType<GlobalModalConfirmFunctions["confirm"]> =>
-  getGlobalModalConfirmFuncs().confirm(...args);
-export const confirmBeforeAction = (
-  ...args: Parameters<GlobalModalConfirmFunctions["confirmBeforeAction"]>
-): ReturnType<GlobalModalConfirmFunctions["confirmBeforeAction"]> =>
-  getGlobalModalConfirmFuncs().confirmBeforeAction(...args);
+  getGlobalModalConfirmFuncs().andThen(({ confirm }) => confirm(...args));
 export const assertConfirm = (
   ...args: Parameters<GlobalModalConfirmFunctions["assertConfirm"]>
 ): ReturnType<GlobalModalConfirmFunctions["assertConfirm"]> =>
-  getGlobalModalConfirmFuncs().assertConfirm(...args);
+  getGlobalModalConfirmFuncs().andThen(({ assertConfirm }) => assertConfirm(...args));
+export function confirmBeforeAction<TArgs extends any[], TOk, TErr extends Error>(
+  options: ConfirmOptions,
+  action: Action<TArgs, TOk, TErr>
+) {
+  return (...args: TArgs): ResultAsync<TOk, TErr | CancelledByUser> =>
+    getGlobalModalConfirmFuncs().andThen(({ assertConfirm }) =>
+      assertConfirm(options).andThen(() => action(...args))
+    );
+}

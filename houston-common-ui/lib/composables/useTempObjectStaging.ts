8428519365ef00f5ad0fc48ab4f63fa280dedd3a
joshuaboud <joshuaboud@hotmail.com>
2024-11-12 19:08:05 -0400

    add optional default value override

--- houston-common-ui/lib/composables/useTempObjectStaging.ts
+++ houston-common-ui/lib/composables/useTempObjectStaging.ts
@@ -1,19 +1,32 @@
-import {ref, toRaw, unref, watch, computed, type Ref, type UnwrapRef} from "vue";
+import { ref, toRaw, unref, watch, computed, type Ref, type UnwrapRef } from "vue";
 import deepEqual from "deep-equal";
 
-export const useTempObjectStaging = <T extends any>(originalObject: Ref<T>) => {
-    const tempObject = ref(structuredClone(toRaw(unref(originalObject))));
+export const useTempObjectStaging = <T extends any>(
+  originalObject: Ref<T>,
+  overrideDefaults?: (_: T) => T
+) => {
+  const tempObject = ref(structuredClone(toRaw(unref(originalObject))));
 
-    watch(originalObject, (newObject) => {
-        tempObject.value = structuredClone(toRaw(newObject)) as UnwrapRef<T>;
-    }, { deep: true });
+  if (overrideDefaults) {
+    tempObject.value = overrideDefaults(tempObject.value as T) as UnwrapRef<T>;
+  }
 
-    const modified = computed<boolean>(() =>
-        !deepEqual(tempObject.value, unref(originalObject)));
+  watch(
+    originalObject,
+    (newObject) => {
+      tempObject.value = structuredClone(toRaw(newObject)) as UnwrapRef<T>;
+      if (overrideDefaults) {
+        tempObject.value = overrideDefaults(tempObject.value as T) as UnwrapRef<T>;
+      }
+    },
+    { deep: true }
+  );
 
-    const resetChanges = () => {
-        tempObject.value = structuredClone(toRaw(unref(originalObject))) as UnwrapRef<T>;
-    };
+  const modified = computed<boolean>(() => !deepEqual(tempObject.value, unref(originalObject)));
 
-    return { tempObject, modified, resetChanges };
+  const resetChanges = () => {
+    tempObject.value = structuredClone(toRaw(unref(originalObject))) as UnwrapRef<T>;
+  };
+
+  return { tempObject, modified, resetChanges };
 };

153e275be9e213c83babfd22914954f0a0f89310
joshuaboud <joshuaboud@hotmail.com>
2025-01-29 17:35:08 -0400

    more cleanup + manager stubs

--- houston-common-lib/lib/managers/zfs/manager.ts
+++ houston-common-lib/lib/managers/zfs/manager.ts
@@ -1,7 +1,23 @@
-import { ZpoolCreateOptions, ZPool, Server, Command, ZPoolBase, VDevBase } from '@/index';
+import { ZpoolCreateOptions, ZPool, Server, Command, ZPoolBase, VDevBase, VDev } from '@/index';
 
 export interface IZFSManager {
     createPool(options: ZpoolCreateOptions): Promise<ZPool>;
     getPools(): Promise<ZPool[]>;
-    addVDevToPool(pool:ZPoolBase, vdev: VDevBase)
+    addVDevToPool(pool:ZPoolBase, vdev: VDevBase): Promise<VDev>;
+}
+
+export class ZFSManager implements IZFSManager {
+    constructor(protected server: Server = new Server()) {}
+
+    async createPool(options: ZpoolCreateOptions): Promise<ZPool> {
+        
+    }
+
+    async getPools(): Promise<ZPool[]> {
+        
+    }
+
+    async addVDevToPool(pool: ZPoolBase, vdev: VDevBase): Promise<VDev> {
+        
+    }
 }

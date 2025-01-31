ca5e2d29d045493d3ab7e2b058949f6517ca1fd3
joshuaboud <joshuaboud@hotmail.com>
2025-01-31 18:23:25 -0400

    missing export

--- houston-common-lib/lib/managers/index.ts
+++ houston-common-lib/lib/managers/index.ts
@@ -2,5 +2,6 @@ export * from "./samba/manager";
 export * from "./samba/types";
 export * from "./samba/parser";
 export * from "./easysetup/manager";
+export * from "./easysetup/types";
 export * from "./zfs/types";
 export * from "./zfs/manager";

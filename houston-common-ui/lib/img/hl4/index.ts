b308dbc42c973838001ab9b862c8274208166a58
joshuaboud <joshuaboud@hotmail.com>
2025-02-14 12:22:37 -0400

    add missing assets from main

---
+++ houston-common-ui/lib/img/hl4/index.ts
@@ -0,0 +1,11 @@
+import overview from './overview.png';
+import drivebay from './drivebay.png';
+
+export const bayLocations: Record<string, { x: number; y: number }> = {
+    "1-1": { x: 9, y: 33},
+    "1-2": { x: 41, y: 33},
+    "1-3": { x: 72, y: 33},
+    "1-4": { x: 104, y: 33},
+}
+
+export { overview, drivebay };

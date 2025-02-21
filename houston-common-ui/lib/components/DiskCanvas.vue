d5049bbbd57606b1b03ffb488e6c758a0b69f28e
joshuaboud <joshuaboud@hotmail.com>
2025-02-21 18:20:51 -0400

    reorganize ServerView and use dynamic import to only load threejs if needed

---
+++ houston-common-ui/lib/components/DiskCanvas.vue
@@ -0,0 +1,92 @@
+<template>
+  <div ref="canvasParent" class="overflow-hidden"></div>
+</template>
+
+<script setup lang="ts">
+import { useTemplateRef, onBeforeUnmount, watchEffect, type WatchHandle } from "vue";
+import { Server, type DriveSlot } from "@45drives/houston-common-lib";
+
+import { useDarkModeState } from "@/composables";
+
+const props = withDefaults(
+  defineProps<{
+    server?: Server;
+    enableSelection: boolean;
+    enableRotate: boolean;
+    enablePan: boolean;
+    enableZoom: boolean;
+  }>(),
+  {
+    server: () => new Server(),
+    enableSelection: false,
+    enableRotate: false,
+    enablePan: false,
+    enableZoom: false,
+  }
+);
+
+let unmountCallback: (() => void) | undefined = undefined;
+
+const canvasParent = useTemplateRef<HTMLDivElement>("canvasParent");
+
+const selectedDriveSlots = defineModel<DriveSlot[]>("selectedDriveSlots", { default: [] });
+
+const driveSlots = defineModel<DriveSlot[]>("driveSlots", { default: [] });
+
+const darkMode = useDarkModeState();
+
+import("./ServerView").then(({ ServerView }) => {
+  const watchHandles: WatchHandle[] = [];
+
+  const serverView = new ServerView(props.server);
+
+  import("./ServerView/DriveSlot").then(({ DriveSlotComponent }) => {
+    serverView.addEventListener("selectionchange", (e) => {
+      selectedDriveSlots.value = e.components
+        .filter(
+          (c): c is InstanceType<typeof DriveSlotComponent> => c instanceof DriveSlotComponent
+        )
+        .map((driveSlot) => driveSlot.userData);
+    });
+  });
+
+  serverView.addEventListener("driveslotchange", (e) => {
+    driveSlots.value = [...e.slots];
+  });
+
+  watchHandles.push(
+    watchEffect(() => {
+      if (!canvasParent.value) {
+        return;
+      }
+      serverView.start(canvasParent.value);
+    })
+  );
+
+  watchHandles.push(
+    watchEffect(() => {
+      serverView.enableSelection = props.enableSelection;
+      serverView.enableRotate = props.enableRotate;
+      serverView.enablePan = props.enablePan;
+      serverView.enableZoom = props.enableZoom;
+    })
+  );
+
+  watchHandles.push(
+    watchEffect(() => {
+      serverView.setBackground(darkMode.value ? 0x262626 : 0xffffff);
+    })
+  );
+
+  unmountCallback = () => {
+    serverView.stop();
+    for (const wh of watchHandles) {
+      wh.stop();
+    }
+  };
+});
+
+onBeforeUnmount(() => {
+  unmountCallback?.();
+});
+</script>
--- houston-common-ui/lib/components/disks/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as DiskCanvas } from "./DiskCanvas.vue";

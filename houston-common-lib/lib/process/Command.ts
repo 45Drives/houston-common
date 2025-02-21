cbfcaf46e9fa3aa51b4564906cd5ddf089a1c084
joshuaboud <joshuaboud@hotmail.com>
2025-02-21 17:00:52 -0400

    fix command naming

--- houston-common-lib/lib/process/Command.ts
+++ houston-common-lib/lib/process/Command.ts
@@ -3,6 +3,7 @@ export type CommandOptions = {
   environ?: Record<string, string>;
   pty?: boolean;
   superuser?: "try" | "require";
+  arg0?: string;
 };
 
 export class Command {
@@ -15,7 +16,7 @@ export class Command {
   }
 
   public getName(): string {
-    return this.argv[0] ?? "";
+    return this.options.arg0 ?? this.argv[0] ?? "";
   }
 
   public toString(): string {
@@ -24,14 +25,15 @@ export class Command {
 }
 
 export class BashCommand extends Command {
-  constructor(script: string, args: string[] = [], opts: CommandOptions & { arg0?: string } = {}) {
-    const arg0 = opts.arg0 ?? "HoustonBashCommand";
-    super(["/usr/bin/env", "bash", "-c", script, arg0, ...args], opts);
+  constructor(script: string, args: string[] = [], opts: CommandOptions = {}) {
+    opts.arg0 ??= "HoustonBashCommand";
+    super(["/usr/bin/env", "bash", "-c", script, opts.arg0, ...args], opts);
   }
 }
 
 export class PythonCommand extends Command {
   constructor(script: string, args: string[] = [], opts: CommandOptions = {}) {
+    opts.arg0 ??= "HoustonPythonCommand";
     super(["/usr/bin/env", "python3", "-c", script, ...args], opts);
   }
 }

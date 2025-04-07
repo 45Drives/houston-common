978e3503ad0df24b97ed156570eb20cbce0ea2d3
JordanKeo45 <jkeough@45drives.com>
2025-04-07 13:27:16 -0300

    Adds DynamicBrandingLogo component to dynamically show proper 45Drives logo based on division string prop

---
+++ houston-common-ui/lib/img/branding/index.ts
@@ -0,0 +1,6 @@
+import base_logo from './45drives.png';
+import enterprise_logo from './45enterprise.png';
+import homelab_logo from './45homelab.png';
+import pro_logo from './45professional.png';
+
+export { base_logo, enterprise_logo, homelab_logo, pro_logo};

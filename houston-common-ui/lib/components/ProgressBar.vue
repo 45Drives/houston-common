23d72c71156ca48db713187b87b8917a1df5a7c5
joshuaboud <joshuaboud@hotmail.com>
2025-04-22 16:56:43 -0300

    extract ProgressBar from wizard

--- houston-common-ui/lib/components/ProgressBar.vue
+++ houston-common-ui/lib/components/ProgressBar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-full bg-well rounded-md h-6 relative overflow-hidden">
+  <div class="bg-well h-6 relative overflow-hidden">
     <div
       class="h-full bg-green-500 transition-all duration-300"
       :style="{ width: percent + '%' }"
     ></div>
     <span class="absolute inset-0 flex items-center justify-center text-sm font-semibold text-default dark:text-white">
-      {{ percent }}%
+      {{ Math.round(percent) }}%
     </span>
   </div>
 </template>

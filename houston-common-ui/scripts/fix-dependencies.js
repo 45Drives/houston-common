45Drives/houston-common

commit 1c98fe3820e42aa90992b983d6d4f2413147e84b

added preinstall script to switch deps to npm version and yarn versions if using npm or yarn.

houston-common-ui/scripts/fix-dependencies.js
@@ -1,0 +1,22 @@
+// scripts/fix-dependencies.js
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.resolve();
+const packageJsonPath = path.join(__dirname, 'package.json');
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+// Check if Yarn is being used
+const isYarn = fs.existsSync(path.join(__dirname, '../.yarnrc'));
+
+// Update dependencies accordingly
+if (isYarn) {
+  packageJson.dependencies['@45drives/houston-common-css'] = 'workspace:^';
+  packageJson.dependencies['@45drives/houston-common-lib'] = 'workspace:^';
+} else {
+  packageJson.dependencies['@45drives/houston-common-css'] = 'file:../houston-common-css';
+  packageJson.dependencies['@45drives/houston-common-lib'] = 'file:../houston-common-lib';
+}
+
+// Write the updated package.json back
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));

commit ce261b32117d1b483f3677d328c51807510bcd1e

fix build issue introduced by 1c98fe3

houston-common-ui/scripts/fix-dependencies.js
@@ -7,7 +7,7 @@ const packageJsonPath = path.join(__dirname, 'package.json');
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
 
 // Check if Yarn is being used
-const isYarn = fs.existsSync(path.join(__dirname, '../.yarnrc'));
+const isYarn = process.env["npm_config_user_agent"]?.includes("yarn") ?? false;
 
 // Update dependencies accordingly
 if (isYarn) {
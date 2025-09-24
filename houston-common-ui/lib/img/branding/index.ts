738e1a216c111c27b3088c38a917cde60748a6e5
JordanKeo45 <jkeough@45drives.com>
2025-09-24 15:11:33 -0300

    adds 45Studio branding

--- houston-common-ui/lib/img/branding/index.ts
+++ houston-common-ui/lib/img/branding/index.ts
@@ -2,9 +2,11 @@ import base_logo from './45drives.png';
 import enterprise_logo from './45enterprise.png';
 import homelab_logo from './45homelab.png';
 import pro_logo from './45professional.png';
+import studio_logo from './45studio.png';
 import base_logo_dark from './45drives-darkmode.png';
 import enterprise_logo_dark from './45enterprise-darkmode.png';
 import homelab_logo_dark from './45homelab-darkmode.png';
 import pro_logo_dark from './45professional-darkmode.png';
+import studio_logo_dark from './45studio-darkmode.png';
 
-export { base_logo, enterprise_logo, homelab_logo, pro_logo, base_logo_dark, enterprise_logo_dark, homelab_logo_dark, pro_logo_dark};
+export { base_logo, enterprise_logo, homelab_logo, pro_logo, studio_logo, base_logo_dark, enterprise_logo_dark, homelab_logo_dark, pro_logo_dark, studio_logo_dark};

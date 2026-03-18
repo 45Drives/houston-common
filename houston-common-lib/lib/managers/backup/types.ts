8bdc156d05607c23cfcdbd2a8820da9cb34181cc
JordanKeo45 <jkeough@45drives.com>
2026-03-18 09:52:52 -0300

    updates backuptask type to handle files in addition to folders

--- houston-common-lib/lib/managers/backup/types.ts
+++ houston-common-lib/lib/managers/backup/types.ts
@@ -7,10 +7,11 @@ export interface TaskSchedule {
 export interface BackUpTask {
   schedule: TaskSchedule
   description: string     // Unique description of the task. Also programatically add ID (Houston-backup-task) so we can query
-  source: string          // client folder to backup
+  source: string          // client folder or file to backup
   target: string          // mount point for backup location(preappened clientID(client hostname))
   mirror: boolean
   uuid: string
+  isFile?: boolean        // true when backing up a single file rather than a directory
   status?:
   | 'online'
   | 'offline_unreachable'

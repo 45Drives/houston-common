b3704cdb42703eca012b7f0208fd9c06d1ff2559
JordanKeo45 <jkeough@45drives.com>
2026-03-18 10:24:35 -0300

    revert backup file changes (only backup folders again)

--- houston-common-lib/lib/managers/backup/types.ts
+++ houston-common-lib/lib/managers/backup/types.ts
@@ -7,11 +7,10 @@ export interface TaskSchedule {
 export interface BackUpTask {
   schedule: TaskSchedule
   description: string     // Unique description of the task. Also programatically add ID (Houston-backup-task) so we can query
-  source: string          // client folder or file to backup
+  source: string          // client folder to backup
   target: string          // mount point for backup location(preappened clientID(client hostname))
   mirror: boolean
   uuid: string
-  isFile?: boolean        // true when backing up a single file rather than a directory
   status?:
   | 'online'
   | 'offline_unreachable'
